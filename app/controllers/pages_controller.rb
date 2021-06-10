eed6beeb6c51e0db75a48679611e37e4c55c350d
dean <deanraguso@gmail.com>
2021-06-11 08:51:20 +1000

    Created: Base template for dashboard pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+  before_action :unauthorized_redirect, only: [:dashboard]
+
   def index
   end
+
+  def dashboard
+  end
+
+  private
+  def unauthorized_redirect
+    unless current_user
+      redirect_to root_path, notice: "You must be logged in to access projects!"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :projects
   devise_for :users
   root 'pages#index'
+  get 'dashboard', to: "pages#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
